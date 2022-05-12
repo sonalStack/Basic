b96a1392a9ec6e7b481e0cc7c9a57887ca755e09
sonalStack <sumersonali@gamil.com>
2022-05-12 13:22:15 +0530

    basic java code

---
+++ src/Assginment/febonicc.java
@@ -0,0 +1,34 @@
+package Assginment;
+
+public class febonicc {
+	
+		
+	public static void main(String[]args) {
+			
+			int a=0,b=1,c=10;
+			//int b=1;
+			//int c=10;
+
+			System.out.println("fibonacci series upto "+ c +" are: ");
+			
+			
+			int d= 1;// to add one more to next no in loop
+			int e;
+			while(d<=c)// 
+			{
+			
+				System.out.print(" "+a);
+				e=a+b;//0+1=1,1+1=2,1+2=3,2+3=5,3+5=8,8+5=13,13+8=21,21+13=34
+			     a=b;//
+				b=e;//
+			
+				d++;// now d=2
+				 
+				
+			}
+
+
+	}}
+
+
+}
